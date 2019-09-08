7e25fc3990f6107223872f5296b6d299c9304c48
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-08 22:21:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,7 +13,7 @@ def consolidate_cart(cart)
       #binding.pry
       cart_hash.merge!(temp_hash)
       cart_hash[:element_pair.keys.store(:count, 1)]
-      gp cart_hash
+      p cart_hash
     end
   end
 cart_hash
