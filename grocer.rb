7dd5fbd19a02b61097ec17bbb108ff3377cbbd48
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-08 22:53:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -6,22 +6,21 @@ def consolidate_cart(cart)
 
   cart.each do |element_pair|
 
-    hash_key = element_pair.keys[0]
+    hash_key = :element_pair.keys[0]
     hash_value = element_pair.values[0]
-    hash_price = Hash[*hash_value.to_a.at(0)]
-    hash_clearance = Hash[*hash_value.to_a.at(1)]
+
     binding.pry
 
-    #if cart_hash.has_key?(element_pair.keys)
-    #  cart_hash[:element_pair.keys[0]][:count] =+ 1
+    if cart_hash.has_key?(hash_key)
+      cart_hash[:hash_key][:count] =+ 1
 
-  #  else
-    #  temp_hash[element_pair.keys[0]] = element_pair.values[0][0]
+    else
+      temp_hash[element_pair.keys[0]] = element_pair.values[0][0]
 
-    #  cart_hash.merge!(temp_hash)
+      cart_hash.merge!(temp_hash)
 
-    #  cart_hash[:element_pair.keys[0]].store(:count, 1)
-  #  end
+      cart_hash[:element_pair.keys[0]].store(:count, 1)
+    end
   end
 cart_hash
 end
